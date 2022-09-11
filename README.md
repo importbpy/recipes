80bd16b46de47eeef2b0ebc378393366c00c9222
Marek <moravecma@seznam.cz>
2022-09-11 10:21:54 +0200

    Sentry

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # recipes
+
+
+
+# run composer install as www-data user
+
+`sudo su -l www-data -s /bin/bash -c "cd $PWD; composer install"`
--- src/Model/User/UserRepository.php
+++ src/Model/User/UserRepository.php
@@ -3,7 +3,7 @@
 namespace App\Model\User;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
